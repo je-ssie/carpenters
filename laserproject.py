70e1ae35b0d5726a7ce0c07283586039a0bac894
jessie <133827193+je-ssie@users.noreply.github.com>
2026-03-24 16:14:21 -0400

    Add files via upload
    starting file (there's not much in this)

---
+++ laserproject.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 15:42:30 2026
+
+@author: jessie
+"""
+
+class Reflect:
+    
+    def __init__(self, position):
+        self.pos = position
+        
