980a1ec98c3a6f4dc8d35c7cf063917e04631691
Jessie Huh <jessie@Jessies-MacBook-Pro.local>
2026-04-05 16:21:53 -0400

    Revert "wrote _generate_configs(), build_block_lookup(), rewrote check_collision(), get_configurations(), laser_trace() to avoid loops, for optimization. solves all puzzles except tiny_5? pretty quick solve now :)"
    This reverts commit 815353ff3cbf7eb298be1dd6f5f39c49327974d5.

--- laser_test.py
+++ laser_test.py
@@ -6,7 +6,7 @@ Created on Thu Apr  2 16:25:10 2026
 @author: jessie
 """
 import unittest
-from laserproject import *
+from laser_personal import *
 
 # # test get all face
 # b = Block([1, 2], False)
